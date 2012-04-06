6c61fd3b8f00fb5b5c0d474458b5bbfffcc31cee
Ignace Mouzannar <ignace.mouzannar@enovance.com>
2012-04-06 22:51:37 +0200

    monit: temporarely commented the add_service function

--- monit/manifests/add_service.pp
+++ monit/manifests/add_service.pp
@@ -1,12 +1,12 @@
-define monit::add_service()
-{
-  file { $name:
-    ensure  => present,
-    path    => "$monit::config::monitd/${name}",
-    content => template("monit/conf.d/${name}.erb"),
-    group   => root,
-    require => File[$monit::config::confdir],
-    notify  => Class['monit::services'],
-    mode    => '0700';
-  }
-}
+#define monit::add_service()
+#{
+#  file { $name:
+#    ensure  => present,
+#    path    => "$monit::config::monitd/${name}",
+#    content => template("monit/conf.d/${name}.erb"),
+#    group   => root,
+#    require => File[$monit::config::confdir],
+#    notify  => Class['monit::services'],
+#    mode    => '0700';
+#  }
+#}
